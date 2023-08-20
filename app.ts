f2b6477f9feae10b698695ef569745cfe73e4e19
Paul Zamshin <p.zamshin@mobility.hyundai.ru>
2023-08-20 13:05:45 +0900

    launch succeed

--- app.ts
+++ app.ts
@@ -1,13 +1,13 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+var indexRouter = require('./routes/index.ts');
+var usersRouter = require('./routes/users.ts');
 
-const app = express();
+var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
